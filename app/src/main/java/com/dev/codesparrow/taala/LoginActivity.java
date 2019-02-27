35e5be2cf12485ee139a1f69ed5ad5b18ea135b9
Feny Roy <FenyRoy@users.noreply.github.com>
2019-02-27 16:43:56 +0530

    System Change

--- app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
+++ app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
@@ -1,18 +1,34 @@
 package com.dev.codesparrow.taala;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button LoginButton;
+    EditText UidText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText t = findViewById(R.id.UidText);
-        t.setGravity(Gravity.CENTER);
+        UidText = findViewById(R.id.UidText);
+
+        LoginButton = findViewById(R.id.LgnBtn);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent OtpIntent = new Intent(LoginActivity.this,OtpActivity.class);
+                startActivity(OtpIntent);
+
+            }
+        });
     }
 }
