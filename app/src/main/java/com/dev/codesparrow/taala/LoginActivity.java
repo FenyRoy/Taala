f2bde298133f0c9bd027f69defd68e49fe752e34
Feny Roy <FenyRoy@users.noreply.github.com>
2019-03-02 10:18:59 +0530

    Keypair Generated

--- app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
+++ app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
@@ -3,21 +3,33 @@ package com.dev.codesparrow.taala;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button LoginButton;
     EditText UidText;
+    KeyPair keypair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        keypair = generateKeys();
+
+        final byte[] publicKey = keypair.getPublic().getEncoded();
+        final byte[] privateKey = keypair.getPrivate().getEncoded();
+
 
         UidText = findViewById(R.id.UidText);
 
@@ -29,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
                 if(UidText.getText().toString().isEmpty())
                 {
                     Toast.makeText(LoginActivity.this, "UID Empty", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, publicKey.toString()+" diff "+privateKey.toString(), Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
@@ -39,5 +52,18 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public KeyPair generateKeys() {
+        KeyPair keyPair = null;
+        try {
+            // get instance of rsa cipher
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(4096);            // initialize key generator
+            keyPair = keyGen.generateKeyPair(); // generate pair of keys
+        } catch(GeneralSecurityException e) {
+            System.out.println(e);
+        }
+        return keyPair;
     }
 }
