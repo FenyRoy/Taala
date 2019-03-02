7b8104217bcd51eb00b46c071205790ae1fd246e
Sreeram Venkitesh <thatonephysicsnerd@gmail.com>
2019-03-02 09:39:52 +0530

    Removed toast

--- app/src/main/java/com/dev/codesparrow/taala/MainActivity.java
+++ app/src/main/java/com/dev/codesparrow/taala/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"Test Commit Baby!", Toast.LENGTH_LONG).show();
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(loginIntent);
