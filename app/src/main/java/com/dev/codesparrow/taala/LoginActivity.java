f48d8816f23754abab49bea9d5673b0e41778578
Feny Roy <FenyRoy@users.noreply.github.com>
2019-03-02 13:03:24 +0530

    RSA Encryption

--- app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
+++ app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
@@ -11,29 +11,64 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     Button LoginButton;
     EditText UidText;
-    KeyPair keypair;
+    KeyPairGenerator kpg;
+    KeyPair kp;
+    PublicKey publicKey;
+    PrivateKey privateKey;
+    byte [] encryptedBytes,decryptedBytes;
+    Cipher cipher,cipher1;
+    String encrypted,decrypted,result,ans;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        keypair = generateKeys();
 
-        final byte[] publicKey = keypair.getPublic().getEncoded();
-        final byte[] privateKey = keypair.getPrivate().getEncoded();
 
+        try {
+
+            String input;
+            input= ParseJSON("feny","10-09-19","Pukkunnel House","Roy Paul");
+
+            result = RSAEncrypt(input);
+            Toast.makeText(getBaseContext(), result,Toast.LENGTH_SHORT).show();
+
+
+
+            ans= RSADecrypt(result);
+            System.out.println("Result is"+ans);
+            Toast.makeText(getBaseContext(), ans,Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+
+// TODO Auto-generated catch block
+
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), e.toString(),Toast.LENGTH_LONG).show();
+
+        }
 
         UidText = findViewById(R.id.UidText);
 
@@ -43,14 +78,11 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(UidText.getText().toString().isEmpty())
-                {
+                if (UidText.getText().toString().isEmpty()) {
                     Toast.makeText(LoginActivity.this, "UID Empty", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(LoginActivity.this, publicKey.toString()+" diff "+privateKey.toString(), Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Intent OtpIntent = new Intent(LoginActivity.this,OtpActivity.class);
+                    Toast.makeText(LoginActivity.this, result, Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent OtpIntent = new Intent(LoginActivity.this, OtpActivity.class);
                     startActivity(OtpIntent);
                 }
 
@@ -59,32 +91,51 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public KeyPair generateKeys() {
-        KeyPair keyPair = null;
+    public String ParseJSON(String name, String dob, String address, String father) {
         try {
-            // get instance of rsa cipher
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-            keyGen.initialize(4096);            // initialize key generator
-            keyPair = keyGen.generateKeyPair(); // generate pair of keys
-        } catch(GeneralSecurityException e) {
-            System.out.println(e);
-        }
-        return keyPair;
-
-    public String ParseJSON(String name,String dob,String address,String father){
-        try{
 
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("name",name);
-            jsonObject.put("dob",dob);
-            jsonObject.put("address",address);
-            jsonObject.put("father",father);
+            jsonObject.put("name", name);
+            jsonObject.put("dob", dob);
+            jsonObject.put("address", address);
+            jsonObject.put("father", father);
 
             return jsonObject.toString();
 
-        }catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return null;
+
     }
+
+    public String RSAEncrypt(final String plain) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    {
+        kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(4096);
+        kp = kpg.genKeyPair();
+        publicKey = kp.getPublic();
+        privateKey = kp.getPrivate();
+
+        cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        encryptedBytes = cipher.doFinal(plain.getBytes());
+        encrypted = new String(encryptedBytes);
+        System.out.println("EEncrypted?????"+encrypted);
+        return encrypted;
+
+    }
+
+    public String RSADecrypt (final String result) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    {
+
+        cipher1=Cipher.getInstance("RSA");
+        cipher1.init(Cipher.DECRYPT_MODE, privateKey);
+        decryptedBytes = cipher1.doFinal(result.getBytes());
+        decrypted = new String(decryptedBytes);
+        System.out.println("DDecrypted?????"+decrypted);
+        return decrypted;
+
+    }
+
 }
