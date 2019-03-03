eec517e4059c102bdedc897d7f8123e63c2b1268
Mathew Varghese <mathewvarghesemanu@gmail.com>
2019-03-03 08:57:47 +0530

    UI Update

---
+++ app/src/main/java/com/dev/codesparrow/taala/RegisterSuccessActivity.java
@@ -0,0 +1,13 @@
+package com.dev.codesparrow.taala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_success);
+    }
+}
