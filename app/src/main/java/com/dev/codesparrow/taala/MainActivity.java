9452656105980e30b13d6d23e0e01fc4c3035334
Mathew Varghese <mathewvarghesemanu@gmail.com>
2019-03-03 08:18:34 +0530

    Merge branch 'master' of https://www.github.com/FenyRoy/Taala
    # Conflicts:
    #	.idea/caches/gradle_models.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/dev/codesparrow/taala/LoginActivity.java
    UI Update

--- app/src/main/java/com/dev/codesparrow/taala/MainActivity.java
+++ app/src/main/java/com/dev/codesparrow/taala/MainActivity.java
@@ -16,13 +16,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.kenai.jffi.Main;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
